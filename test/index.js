c49b9493c11da41965cd2357c638c9daff33fc1b
Nate Goldman <nate@ngoldman.me>
2015-03-20 12:18:50 -0700

    add complex multiline & multipolygon collection tests

--- test/index.js
+++ test/index.js
@@ -1,6 +1,8 @@
 var esriExtent = require('..')
 var example = require('./geojson-spec-example.json')
 var exampleMulti = require('./geojson-multi-spec-example.json')
+var portlandParksLines = require('./portland-parks-lines.json')
+var portlandParksPolygons = require('./portland-parks-polygons.json')
 var test = require('tape')
 var expected = {
   xmin: 100,
@@ -12,7 +14,6 @@ var expected = {
     latestWkid: 4326
   }
 }
-
 var expectedMulti = {
   xmin: 98,
   ymin: 0,
@@ -23,8 +24,28 @@ var expectedMulti = {
     latestWkid: 4326
   }
 }
+var expectedParkLines = {
+  xmin: -122.87585280244608,
+  ymin: 45.39555387844955,
+  xmax: -122.37527608962372,
+  ymax: 45.65042567778668,
+  spatialReference: {
+    wkid: 4326,
+    latestWkid: 4326
+  }
+}
+var expectedParkPolygons = {
+  xmin: -122.82747609596763,
+  ymin: 45.43165893393241,
+  xmax: -122.37509388958975,
+  ymax: 45.650689022921206,
+  spatialReference: {
+    wkid: 4326,
+    latestWkid: 4326
+  }
+}
 
-test('get correct extent from geojson spec example', function (t) {
+test('basic geojson spec example', function (t) {
   esriExtent(example, function (err, extent) {
     if (err) throw err
 
@@ -33,11 +54,29 @@ test('get correct extent from geojson spec example', function (t) {
   })
 })
 
-test('get correct extent from geojson multi spec example', function (t) {
+test('basic geojson multi spec example', function (t) {
   esriExtent(exampleMulti, function (err, extent) {
     if (err) throw err
 
     t.deepEqual(extent, expectedMulti)
+    t.end()
+  })
+})
+
+test('complex multiline string feature collection', function (t) {
+  esriExtent(portlandParksLines, function (err, extent) {
+    if (err) throw err
+
+    t.deepEqual(extent, expectedParkLines)
+    t.end()
+  })
+})
+
+test('complex multipolygon feature collection', function (t) {
+  esriExtent(portlandParksPolygons, function (err, extent) {
+    if (err) throw err
+
+    t.deepEqual(extent, expectedParkPolygons)
     t.end()
   })
 })
