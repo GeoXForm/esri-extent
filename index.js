1dbf0736ee04d92e8650129daad79dfce8104698
Nate Goldman <nate@ngoldman.me>
2015-03-20 12:35:45 -0700

    wrap simple polygons

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ module.exports = function (collection, callback) {
     }
 
     if (isPolygon || isMultiPolygon) {
-      coords = [coords]
+      if (isPolygon) coords = [coords]
       loop(bbox, coords)
     }
   })
